75aab0a443f36f0e2d48974033fbb46f77a25fc1
Furio Alessio <alefurio@hotmail.it>
2020-10-28 16:40:46 +0100

    aggiunti i dropdown menu com mouseleave e mouseenter

---
+++ main.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+
+    // mouseenter e mouseleave per <li><a href="">Funzionalità</a></li>
+
+    // $('.headerRight ul li:first-child').mouseenter(function(){
+    //     $('.dropdownFunz').addClass('show');
+    // });
+    //
+    // $('.dropdownFunz').mouseleave(function(){
+    //     $('.dropdownFunz').removeClass('show');
+    // });
+    //
+    // // mouseenter e mouseleave per <li><a href="">Abbonamenti</a></li>
+    //
+    // $('.headerRight ul li:nth-child(2)').mouseenter(function(){
+    //     $('.dropdownAbb').addClass('show');
+    // });
+    //
+    // $('.dropdownAbb').mouseleave(function(){
+    //     $('.dropdownAbb').removeClass('show');
+    // });
+    //
+    //
+    // // mouseenter e mouseleave per <li><a href="">ChiSiamo</a></li>
+    //
+    // $('.headerRight ul li:nth-child(3)').mouseenter(function(){
+    //     $('.dropdownChiSiamo').addClass('show');
+    // });
+    //
+    // $('.dropdownChiSiamo').mouseleave(function(){
+    //     $('.dropdownChiSiamo').removeClass('show');
+    // });
+    //
+    //
+    // // mouseenter e mouseleave per <li><a href="">Aiuto</a></li>
+    //
+    // $('.headerRight ul li:nth-child(4)').mouseenter(function(){
+    //     $('.dropdownAiuto').addClass('show');
+    // });
+    //
+    // $('.dropdownAiuto').mouseleave(function(){
+    //     $('.dropdownAiuto').removeClass('show');
+    // });
+
+
+    $('.menuDrop').each(function(){
+        var indice = $(this).index();
+
+        if (indice == 1){
+
+            $('#funz').mouseenter(function(){
+                    $('.dropdownFunz').addClass('show');
+                    });
+
+            $('.dropdownFunz').mouseleave(function(){
+            $('.dropdownFunz').removeClass('show');
+                    });
+                }
+
+        else if (indice == 2) {
+            $('#abb').mouseenter(function(){
+                    $('.dropdownAbb').addClass('show');
+                    });
+
+            $('.dropdownAbb').mouseleave(function(){
+            $('.dropdownAbb').removeClass('show');
+                    });
+
+        }
+        else if (indice == 3) {
+            $('#chiSiamo').mouseenter(function(){
+                    $('.dropdownChiSiamo').addClass('show');
+                    });
+
+                    $('.dropdownChiSiamo').mouseleave(function(){
+                    $('.dropdownChiSiamo').removeClass('show');
+                            });
+
+        }
+        else if (indice == 4) {
+            $('#aiuto').mouseenter(function(){
+                    $('.dropdownAiuto').addClass('show');
+                    });
+
+                    $('.dropdownAiuto').mouseleave(function(){
+                    $('.dropdownAiuto').removeClass('show');
+                            });
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+});
